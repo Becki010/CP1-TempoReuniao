ebc18392a9763b8f48e8aa54c5a61e3ea2ca3687
gfirmino231-spec <gfirmino231@gmail.com>
2025-09-17 19:45:00 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+function calcular (){
+    // Declaração de variáveis
+    let horaIni = parseInt(document.getElementById('horaIni').value.trim());
+    let minIni = parseInt(document.getElementById('minIni').value.trim());
+    let horaFim = parseInt(document.getElementById('horaFim').value.trim());
+    let minFim = parseInt(document.getElementById('minFim').value.trim());
+
+    // validação de entrada
+    if (
+        isNaN(horaIni)|| isNaN(horaFim) ||
+        isNaN(minIni) || isNaN(minFim) ||
+        horaIni < 0 || horaIni > 23 ||
+        horaFim < 0 || horaFim > 23 ||
+        minIni < 0 || minIni > 59 ||
+        minFim < 0 || minFim > 59
+
+    ){
+        alert('Tente novamente: Horas (0 -23) , Minutos (0-59)');
+        return;
+    }
+
+
+    // transformando tudo em minutos
+    let inicio_min = horaIni * 60 + minIni
+    let fim_min = horaFim * 60 + minFim
+    
+
+    // Caso passe das 23:59
+     if (fim_min < inicio_min){
+        fim_min +=  24 * 60
+    }
+
+     // Calculando a duração
+    let duracao = fim_min - inicio_min
+
+    // Calculando duração em horas
+    let horas = Math.floor(duracao / 60)
+    let minutos = (duracao % 60)
+
+    // Se horas for < 10
+    if(horas < 10 ) horas = '0' + horas
+    if(minutos < 10) minutos = '0' + minutos 
+
+    // Para aparecer no html
+    document.getElementById('horas').innerText = horas;
+    document.getElementById('minutos').innerText = minutos;
+
+
+
+
+
+}
